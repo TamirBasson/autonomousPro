8aad6e5d5e360b0511cdc81a96814f15dcb8e38d
TamirBasson <tamirbasson99@gmail.com>
2025-04-22 21:23:49 +0300

    added map plotter

--- src/my_bringup/setup.py
+++ src/my_bringup/setup.py
@@ -1,25 +1,30 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'my_bringup'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='tamirbasson99@gmail.com',
+    maintainer='tamir',
+    maintainer_email='tamir@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'image_pose_logger = my_bringup.image_pose_logger:main',
+            'live_pose_plotter = my_bringup.live_pose_plotter:main',
         ],
     },
 )
